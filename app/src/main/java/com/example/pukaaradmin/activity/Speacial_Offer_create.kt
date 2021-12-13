727a180157cf9c3ec136c908f1349fa8bf81e056
adnanakhtar2000 <adnanakhtar.10001@gmail.com>
2021-12-13 12:12:27 +0500

    commit

--- app/src/main/java/com/example/pukaaradmin/activity/Speacial_Offer_create.kt
+++ app/src/main/java/com/example/pukaaradmin/activity/Speacial_Offer_create.kt
@@ -1,12 +1,27 @@
 package com.example.pukaaradmin.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pukaaradmin.R
+import com.example.pukaaradmin.databinding.ActivitySpeacialOfferCreateBinding
 
 class Speacial_Offer_create : AppCompatActivity() {
+
+    private lateinit var speacialOfferCreateBinding: ActivitySpeacialOfferCreateBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_speacial_offer_create)
+        speacialOfferCreateBinding = ActivitySpeacialOfferCreateBinding.inflate(layoutInflater)
+        setContentView(speacialOfferCreateBinding.root)
+
+        speacialOfferCreateBinding.addOfferButton.setOnClickListener {
+            val intent = Intent(this , Dashboard::class.java)
+            startActivity(intent)
+        }
+
+        speacialOfferCreateBinding.backArrow.setOnClickListener {
+            val intent = Intent(this , Dashboard::class.java)
+            startActivity(intent)
+        }
     }
 }
